ed48f6c2da8fc60ea3f41c75e93fceaef36cff57
VVain716 <86568389+VVain716@users.noreply.github.com>
2026-07-30 17:16:11 -0400

    Added build files and executable

---
+++ build/CMakeFiles/chess-engine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chess-engine.dir/src/main.cpp.o"
+  "CMakeFiles/chess-engine.dir/src/main.cpp.o.d"
+  "chess-engine"
+  "chess-engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chess-engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/helper.dir/src/pixel.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/helper.dir/src/pixel.cpp.o: \
+  /Users/vedvainateya/c/cppPrograms/chess-engine/src/pixel.cpp \
+  /Users/vedvainateya/c/cppPrograms/chess-engine/include/pixel.hpp
